ca1e6f57aac6dcae4143786062bdc64bc37879c9
surendiran04 <surendiran.m2004@gmail.com>
2024-05-27 14:02:39 +0530

    Login page - changed

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -11,7 +11,7 @@ const Login = ({ role }) => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const [error, setError] = useState('');
-    const [loggedIn, setLoggedIn] = useState(false);
+    const [loggedIn, setLoggedIn] = useState(true);
 
     const [products, setProducts] = useState([]);
 
@@ -30,29 +30,29 @@ const Login = ({ role }) => {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        try {
-            let endpoint = '';
-            if (role === 'customer') {
-                endpoint = 'http://localhost:3001/auth/customer-login';
-            } else if (role === 'admin') {
-                endpoint = 'http://localhost:3001/auth/admin-login';
-            }
-            const response = await axios.post(endpoint, { username, password });
-            console.log(response);
-            setLoggedIn(response);
-        } catch (error) {
-            if (error.response) {
-                if (error.response.status === 500) {
-                    setError('Internal server error. Please try again later.');
-                } else {
-                    setError('Invalid credentials');
-                }
-            } else if (error.request) {
-                setError('No response received from the server. Please check your connection.');
-            } else {
-                setError('Error sending data: ' + error.message);
-            }
-        }
+        // try {
+        //     let endpoint = '';
+        //     if (role === 'customer') {
+        //         endpoint = 'http://localhost:3001/auth/customer-login';
+        //     } else if (role === 'admin') {
+        //         endpoint = 'http://localhost:3001/auth/admin-login';
+        //     }
+        //     const response = await axios.post(endpoint, { username, password });
+        //     console.log(response);
+        //     setLoggedIn(response);
+        // } catch (error) {
+        //     if (error.response) {
+        //         if (error.response.status === 500) {
+        //             setError('Internal server error. Please try again later.');
+        //         } else {
+        //             setError('Invalid credentials');
+        //         }
+        //     } else if (error.request) {
+        //         setError('No response received from the server. Please check your connection.');
+        //     } else {
+        //         setError('Error sending data: ' + error.message);
+        //     }
+        // }
     };
 
     if (loggedIn) {
